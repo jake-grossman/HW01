082e088fe6d3e21818272aee06dc7648a6f9e010
jake-grossman <123407057+jake-grossman@users.noreply.github.com>
2023-02-03 16:05:06 -0800

    Add files via upload

--- Prob3.py
+++ Prob3.py
@@ -1,8 +1,8 @@
 #====================================================
 # Filename: Prob3.py
 # 
-# Your name:
-# Who did you work with (if anyone)?:
+# Your name: Jake Grossamn
+# Who did you work with (if anyone)?:none
 # Estimate for time spent (in hrs)?:
 #====================================================
 
@@ -13,17 +13,46 @@ def print_divisible_by_six_or_seven():
     but not both. One number printed per line, and nothing is returned.
     """
     # Add your code here!
+    n = 0
+    while n<=100:
+        if n%7==0 or n%6==0:
+            if n%7!=0 and n%6==0:    
+                print(f"this number is a multiple of 7 or 6, but not both: {n}")
+            if n%7==0 and n%6!=0:
+                print(f"this number is a multiple of 7 or 6, but not both: {n}")
+        n += 1
+    
 
 
 
-
-def list_divisible_by_six_or_seven(): # You'll need to add some parameters here
+def list_divisible_by_six_or_seven(min,max): # You'll need to add some parameters here
     """
     Takes two inputs to determine the minumum and maximum values in a range, and
     then returns a list of all the values between those numbers (including the ends)
     which are evenly divisible by 6 or 7, but not both.
     """
     # Add your code here!
+    min = int(input("pick a minimum number for your range between 1 and 100:"))
+    max = int(input("pick a maximum number for your range between 1 and 100:"))
+    a = []
+    n = min
+
+    while n <= max:
+        if n % 7 == 0 or n % 6 == 0:
+            if n % 7 != 0 and n % 6 == 0:    
+                a = a + [n]
+            if n % 7 == 0 and n % 6 != 0:
+                a = a + [n]
+
+        n = n + 1
+
+    return a
+
+            
+
+    
+
+
 
 
 
@@ -39,7 +68,7 @@ def list_divisible_by_six_or_seven(): # You'll need to add some parameters here
 if __name__ == '__main__':
     print('Result of print_divisible_by_six_or_seven:')
     print_divisible_by_six_or_seven()
-    # print('Result of list_divisible_by_six_or_seven:') # You can uncomment this once you get to Part B
-    # print(list_divisible_by_six_or_seven(1,150)) # You can uncomment this once you get to Part B
+    print('Result of list_divisible_by_six_or_seven:') # You can uncomment this once you get to Part B
+    print(list_divisible_by_six_or_seven(1,150)) # You can uncomment this once you get to Part B
 
 
